76cac35cbdd4e8e84903357dae777eff4185f0af
hJJANG <dacapolife87@gmail.com>
2020-07-10 07:51:30 +0900

    빈 등록방식 변경

--- src/main/java/org/hjjang/DIFramework.java
+++ src/main/java/org/hjjang/DIFramework.java
@@ -1,10 +1,9 @@
 package org.hjjang;
 
-import org.hjjang.core.bean.BeanLoader;
+import org.hjjang.core.bean.BeanManager;
 
 public class DIFramework {
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-        BeanLoader beanLoader = new BeanLoader();
-        beanLoader.beanProcessor();
+        BeanManager.beanProcessor();
     }
 }
